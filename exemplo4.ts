f00ff6f560a8202271630f96b106d32308c7fdb1
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-04-21 18:01:53 -0300

    Atualização | Or | Ingresso

--- exemplo4.ts
+++ exemplo4.ts
@@ -1,8 +1,9 @@
-const temIngresso:boolean = false;
+const temIngresso:boolean = true;
 let Idade:number = 16;
 const minimo:number = 16;
+const estaComPais:boolean = false;
 
-if (temIngresso && Idade >= minimo) {
+if (temIngresso && Idade >= minimo || temIngresso && estaComPais) {
     console.log("Pode Entrar!");
 } else if (!temIngresso && Idade >= minimo) {
     console.log("Vai comprar o Ingresso vacilão!");
